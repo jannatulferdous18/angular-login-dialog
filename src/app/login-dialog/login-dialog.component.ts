5d216f9d4b6628d18c83b09760904b86f2654452
Jannatul Ferdous <48725864+jannatulferdous18@users.noreply.github.com>
2024-06-29 17:02:29 +0200

    Add files via upload

---
+++ src/app/login-dialog/login-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-dialog',
+  templateUrl: './login-dialog.component.html',
+  styleUrls: ['./login-dialog.component.css']
+})
+export class LoginDialogComponent {
+  loginForm: FormGroup;
+  hide = true;
+
+  constructor(
+    public dialogRef: MatDialogRef<LoginDialogComponent>,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Perform login action here
+      this.dialogRef.close(this.loginForm.value);
+    }
+  }
+}
